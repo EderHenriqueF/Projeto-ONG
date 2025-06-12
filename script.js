e6e3d234deec61044a06abc09f20462f149b77fd
Eder Henrique de Souza Filho <souzafilhoeder@gmail.com>
2025-06-12 16:39:52 -0300

    Este script contém toda a lógica JavaScript da aplicação, incluindo Navegação entre seções, Formulário de cadastro de necessidades, Listagem e filtragem de oportunidades e Efeitos visuais e animações

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * ConectaVoluntários - Aplicação para conectar voluntários a instituições
+ * 
+ * Este script contém toda a lógica JavaScript da aplicação, incluindo:
+ * - Navegação entre seções
+ * - Formulário de cadastro de necessidades
+ * - Listagem e filtragem de oportunidades
+ * - Efeitos visuais e animações
+ */
